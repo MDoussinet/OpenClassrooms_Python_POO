dbec5f252f7ae08defe52fe447f8c8cbd0753e76
MDoussinet <martin.doussinet@epitech.eu>
2024-03-18 15:14:56 +0100

    feat(py): adding part1 chapter3 exercice

---
+++ p1C3_toolbox.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+class ToolBox:
+
+    def __init__(self):
+        self.tools = []
+
+    def add_tool(self, tool):
+        pass
+
+    def remove_tool(self, tool):
+        pass
+
+
+class Screwdriver:
+
+    def __init__(self, size):
+        self.size = size
+
+    def tighten(self, screw):
+        pass
+
+    def loosen(self, screw):
+        pass
+
+
+class Hammer:
+
+    def __init__(self, color="red"):
+        self.color = color
+
+    def paint(self, color):
+        pass
+
+    def hammer_in(self, nail):
+        pass
+
+    def remove(self, nail):
+        pass
